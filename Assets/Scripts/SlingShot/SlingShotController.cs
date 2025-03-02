8f6b6a6f2c968b16d1aba03dad506407ace6b4f2
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-03-02 12:36:58 +0530

    working with slingshot

--- Assets/Scripts/SlingShot/SlingShotController.cs
+++ Assets/Scripts/SlingShot/SlingShotController.cs
@@ -1,18 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SlingShotController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Camera _mainCamera;
+
+    [SerializeField] private LineRenderer _leftLineRenderer;
+    [SerializeField] private LineRenderer _rightLineRenderer;
+    [SerializeField] private Transform _leftStartTransform;
+    [SerializeField] private Transform _rightStartTransform;
+    [SerializeField] private Transform _centerPosition;
+    [SerializeField] private Transform _idlePosition;
+
+    [SerializeField] private float _maxDistance = 3.5f;
+
+    private Vector2 _slingShotLinesPosition;
+
+    private void Update()
     {
-        
+        if (Mouse.current.leftButton.isPressed)
+        {
+            DrawSlingShot();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DrawSlingShot()
     {
-        
+        Vector3 touchPosition = _mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+        _slingShotLinesPosition = _centerPosition.position + Vector3.ClampMagnitude(touchPosition - _centerPosition.position, _maxDistance);
+        SetLines(_slingShotLinesPosition);
+    }
+
+    private void SetLines(Vector2 position)
+    {
+        _leftLineRenderer.SetPosition(0, position);
+        _leftLineRenderer.SetPosition(1, _leftStartTransform.position);
+
+        _rightLineRenderer.SetPosition(0, position);
+        _rightLineRenderer.SetPosition(1, _rightStartTransform.position);
     }
 }
