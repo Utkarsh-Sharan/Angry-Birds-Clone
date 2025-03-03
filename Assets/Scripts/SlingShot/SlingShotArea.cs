651d5fd87b6fdb45bab57da36f1415b41732e3a2
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-03-03 10:43:32 +0530

    implemented mouse area for sling shot

---
+++ Assets/Scripts/SlingShot/SlingShotArea.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SlingShotArea : MonoBehaviour
+{
+    [SerializeField] private Camera _mainCamera;
+    [SerializeField] private LayerMask _slingShotAreaMask;
+
+    public bool IsWithinSlingShotArea()
+    {
+        Vector2 worldPosition = _mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+        if (Physics2D.OverlapPoint(worldPosition, _slingShotAreaMask))
+            return true;
+        
+        return false;
+    }
+}
--- Assets/Scripts/SlingShot/SlingShotController.cs
+++ Assets/Scripts/SlingShot/SlingShotController.cs
@@ -6,29 +6,54 @@ using UnityEngine.InputSystem;
 
 public class SlingShotController : MonoBehaviour
 {
+    [Header("Camera")]
     [SerializeField] private Camera _mainCamera;
 
+    [Header("Line Renderers")]
     [SerializeField] private LineRenderer _leftLineRenderer;
     [SerializeField] private LineRenderer _rightLineRenderer;
+
+    [Header("Transform References")]
     [SerializeField] private Transform _leftStartTransform;
     [SerializeField] private Transform _rightStartTransform;
     [SerializeField] private Transform _centerPosition;
     [SerializeField] private Transform _idlePosition;
 
+    [Header("Scripts")]
+    [SerializeField] private SlingShotArea _slingShotArea;
+
+    [Header("Sling Shot Data")]
     [SerializeField] private float _maxDistance = 3.5f;
 
     private Vector2 _slingShotLinesPosition;
+    private bool _isClickedWithinArea;
+
+    private void Awake()
+    {
+        _leftLineRenderer.enabled = false;
+        _rightLineRenderer.enabled = false;
+    }
 
     private void Update()
     {
-        if (Mouse.current.leftButton.isPressed)
-        {
+        if(Mouse.current.leftButton.wasPressedThisFrame && _slingShotArea.IsWithinSlingShotArea())
+            _isClickedWithinArea = true;
+
+        if(Mouse.current.leftButton.isPressed && _isClickedWithinArea)
             DrawSlingShot();
-        }
+
+        if(Mouse.current.leftButton.wasReleasedThisFrame)
+            _isClickedWithinArea = false;
     }
 
     private void DrawSlingShot()
     {
+        if(!_leftLineRenderer.enabled && !_rightLineRenderer.enabled)
+        {
+            _leftLineRenderer.enabled = true;
+            _rightLineRenderer.enabled = true;
+        }
+
         Vector3 touchPosition = _mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
 
         _slingShotLinesPosition = _centerPosition.position + Vector3.ClampMagnitude(touchPosition - _centerPosition.position, _maxDistance);
